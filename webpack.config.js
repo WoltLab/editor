3cd287a66dedff23ae6f7219025b4a40e01111ba
Alexander Ebert <ebert@woltlab.com>
2023-02-22 15:28:38 +0100

    Optimize the output for production builds

--- webpack.config.js
+++ webpack.config.js
@@ -5,67 +5,77 @@
 const path = require("path");
 const { styles } = require("@ckeditor/ckeditor5-dev-utils");
 
-module.exports = {
-  // https://webpack.js.org/configuration/entry-context/
-  entry: "./app.ts",
+module.exports = (env, argv) => {
+  let filename = "ckeditor5.min.js";
+  if (argv.mode === "development") {
+    filename = "ckeditor5.debug.js";
+  }
 
-  // https://webpack.js.org/configuration/output/
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    library: "CKEditor5",
-    //libraryTarget: 'amd',
-  },
+  const config = {
+    // https://webpack.js.org/configuration/entry-context/
+    entry: "./app.ts",
 
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node-modules/,
-      },
-      {
-        test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+    // https://webpack.js.org/configuration/output/
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename,
+      library: "CKEditor5",
+      //libraryTarget: 'amd',
+    },
 
-        use: ["raw-loader"],
-      },
-      {
-        test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: "ts-loader",
+          exclude: /node-modules/,
+        },
+        {
+          test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
 
-        use: [
-          {
-            loader: "style-loader",
-            options: {
-              injectType: "singletonStyleTag",
-              attributes: {
-                "data-cke": true,
+          use: ["raw-loader"],
+        },
+        {
+          test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+
+          use: [
+            {
+              loader: "style-loader",
+              options: {
+                injectType: "singletonStyleTag",
+                attributes: {
+                  "data-cke": true,
+                },
               },
             },
-          },
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: styles.getPostCssConfig({
-                themeImporter: {
-                  themePath: require.resolve("@ckeditor/ckeditor5-theme-lark"),
-                },
-                minify: true,
-              }),
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: styles.getPostCssConfig({
+                  themeImporter: {
+                    themePath: require.resolve("@ckeditor/ckeditor5-theme-lark"),
+                  },
+                  minify: true,
+                }),
+              },
             },
-          },
-        ],
-      },
-    ],
-  },
+          ],
+        },
+      ],
+    },
 
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
 
-  // Useful for debugging.
-  devtool: "source-map",
+    // By default webpack logs warnings if the bundle is bigger than 200kb.
+    performance: { hints: false },
+  }
 
-  // By default webpack logs warnings if the bundle is bigger than 200kb.
-  performance: { hints: false },
+  if (argv.mode === "development") {
+    config.devtool = "source-map";
+  }
+
+  return config;
 };
