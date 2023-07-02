f1db0265412ccedfb555711fc9bd760b46cc5f7f
Alexander Ebert <ebert@woltlab.com>
2023-07-02 12:25:08 +0200

    Code cleanup

--- plugins/ckeditor5-woltlab-magic-paragraph/src/woltlabmagicparagraph.ts
+++ plugins/ckeditor5-woltlab-magic-paragraph/src/woltlabmagicparagraph.ts
@@ -1,6 +1,8 @@
 /**
- * Inserts extra buttons for BBCodes into the toolbar. Buttons have support for
- * Font Awesome icons through the `<fa-icon>` element.
+ * Listens for clicks in the editor that occur within the margin of certain
+ * block elements as well as clicks at the very beginning or end of the editor.
+ * If such a click is detected and there is no paragraph at that location then
+ * a paragraph will implicitly be injected and the caret is being moved into it.
  *
  * @author Alexander Ebert
  * @copyright 2001-2023 WoltLab GmbH
@@ -24,110 +26,102 @@ export class WoltlabMagicParagraph extends Plugin {
   }
 
   init() {
-    this.editor.editing.view.addObserver(ClickObserver);
+    const { mapper, view } = this.editor.editing;
+    const { document, domConverter } = view;
 
-    const viewDocument = this.editor.editing.view.document;
+    view.addObserver(ClickObserver);
 
-    this.listenTo<ViewDocumentClickEvent>(
-      viewDocument,
-      "click",
-      (event, data) => {
-        console.clear();
+    this.listenTo<ViewDocumentClickEvent>(document, "click", (event, data) => {
+      console.clear();
 
-        const { domEvent, domTarget, target } = data;
+      const { domEvent, target } = data;
 
-        // Ignore any click that hits a paragraph or list item, because those
-        // are perfectly accessible anyway.
-        const elements = target.getAncestors();
-        elements.push(target);
-        for (const element of elements) {
-          if (element.is("element", "p") || element.is("element", "li")) {
-            return;
-          }
-        }
-
-        const { clientY } = domEvent;
-        const { domConverter } = this.editor.editing.view;
-
-        // Check if the click occurred before the first element or after the
-        // last element. These elements do not have a margin thus they would
-        // fail to detect in the margin hit check below.
-        const root = this.editor.editing.view.document.getRoot()!;
-        const firstChild = root.getChild(0);
-        if (firstChild === undefined) {
+      // Ignore any click that hits a paragraph or list item, because those
+      // are perfectly accessible anyway.
+      const elements = target.getAncestors();
+      elements.push(target);
+      for (const element of elements) {
+        if (element.is("element", "p") || element.is("element", "li")) {
           return;
         }
+      }
 
-        if (firstChild.is("containerElement")) {
-          const model = this.editor.editing.mapper.toModelElement(firstChild);
-          if (model && WoltlabMagicParagraph.blockModels.includes(model.name)) {
-            const domElement = domConverter.mapViewToDom(firstChild)!;
-            const { top } = domElement?.getBoundingClientRect();
+      const { clientY } = domEvent;
 
-            if (clientY <= top) {
-              event.stop();
+      // Check if the click occurred before the first element or after the
+      // last element. These elements do not have a margin thus they would
+      // fail to detect in the margin hit check below.
+      const root = document.getRoot()!;
+      const firstChild = root.getChild(0);
+      if (firstChild === undefined) {
+        return;
+      }
 
-              this.#insertParagraphBefore(firstChild);
-              return;
-            }
-          }
-        }
+      if (firstChild.is("containerElement")) {
+        const model = mapper.toModelElement(firstChild);
+        if (model && WoltlabMagicParagraph.blockModels.includes(model.name)) {
+          const domElement = domConverter.mapViewToDom(firstChild)!;
+          const { top } = domElement.getBoundingClientRect();
 
-        if (root.childCount > 1) {
-          const lastChild = root.getChild(root.childCount - 1)!;
-
-          if (lastChild.is("containerElement")) {
-            const model = this.editor.editing.mapper.toModelElement(lastChild);
-            if (
-              model &&
-              WoltlabMagicParagraph.blockModels.includes(model.name)
-            ) {
-              const domElement = domConverter.mapViewToDom(lastChild)!;
-              const { bottom } = domElement?.getBoundingClientRect();
-
-              if (clientY >= bottom) {
-                event.stop();
-
-                this.#insertParagraphAfter(lastChild);
-                return;
-              }
-            }
-          }
-        }
-
-        // Find any block element whose margin is possibly being clicked on.
-        for (const candidate of this.#findPossibleBlocks()) {
-          const domElement = domConverter.mapViewToDom(candidate)!;
-          const { bottom, top } = domElement.getBoundingClientRect();
-          const { marginBottom, marginTop } =
-            window.getComputedStyle(domElement);
-
-          const topBoundary = top - parseInt(marginTop);
-          const bottomBoundary = bottom + parseInt(marginBottom);
-
-          if (clientY >= topBoundary && clientY <= top) {
-            this.#insertParagraphBefore(candidate);
-
+          if (clientY <= top) {
             event.stop();
-            return;
-          } else if (clientY >= bottom && clientY <= bottomBoundary) {
-            this.#insertParagraphAfter(candidate);
 
-            event.stop();
+            this.#insertParagraphBefore(firstChild);
             return;
           }
         }
       }
-    );
+
+      if (root.childCount > 1) {
+        const lastChild = root.getChild(root.childCount - 1)!;
+
+        if (lastChild.is("containerElement")) {
+          const model = mapper.toModelElement(lastChild);
+          if (model && WoltlabMagicParagraph.blockModels.includes(model.name)) {
+            const domElement = domConverter.mapViewToDom(lastChild)!;
+            const { bottom } = domElement.getBoundingClientRect();
+
+            if (clientY >= bottom) {
+              event.stop();
+
+              this.#insertParagraphAfter(lastChild);
+              return;
+            }
+          }
+        }
+      }
+
+      // Find any block element whose margin is possibly being clicked on.
+      for (const candidate of this.#findPossibleBlocks()) {
+        const domElement = domConverter.mapViewToDom(candidate)!;
+        const { bottom, top } = domElement.getBoundingClientRect();
+        const { marginBottom, marginTop } = window.getComputedStyle(domElement);
+
+        const topBoundary = top - parseInt(marginTop);
+        const bottomBoundary = bottom + parseInt(marginBottom);
+
+        if (clientY >= topBoundary && clientY <= top) {
+          this.#insertParagraphBefore(candidate);
+
+          event.stop();
+          return;
+        } else if (clientY >= bottom && clientY <= bottomBoundary) {
+          this.#insertParagraphAfter(candidate);
+
+          event.stop();
+          return;
+        }
+      }
+    });
   }
 
   #insertParagraphBefore(element: ContainerElement): void {
+    const { mapper } = this.editor.editing;
+
     // Check if the adjacent element is already paragraph.
     let paragraph: Element | undefined = undefined;
     if (element.previousSibling && element.previousSibling.is("element", "p")) {
-      paragraph = this.editor.editing.mapper.toModelElement(
-        element.previousSibling
-      );
+      paragraph = mapper.toModelElement(element.previousSibling);
     } else {
       paragraph = undefined;
     }
@@ -137,7 +131,7 @@ export class WoltlabMagicParagraph extends Plugin {
         paragraph = writer.createElement("paragraph");
 
         const position = writer.createPositionBefore(
-          this.editor.editing.mapper.toModelElement(element)!
+          mapper.toModelElement(element)!
         );
         writer.insert(paragraph, position);
       }
@@ -147,12 +141,12 @@ export class WoltlabMagicParagraph extends Plugin {
   }
 
   #insertParagraphAfter(element: ContainerElement): void {
+    const { mapper } = this.editor.editing;
+
     // Check if the adjacent element is already paragraph.
     let paragraph: Element | undefined = undefined;
     if (element.nextSibling && element.nextSibling.is("element", "p")) {
-      paragraph = this.editor.editing.mapper.toModelElement(
-        element.nextSibling
-      );
+      paragraph = mapper.toModelElement(element.nextSibling);
     } else {
       paragraph = undefined;
     }
@@ -162,7 +156,7 @@ export class WoltlabMagicParagraph extends Plugin {
         paragraph = writer.createElement("paragraph");
 
         const position = writer.createPositionAfter(
-          this.editor.editing.mapper.toModelElement(element)!
+          mapper.toModelElement(element)!
         );
         writer.insert(paragraph, position);
       }
@@ -172,15 +166,17 @@ export class WoltlabMagicParagraph extends Plugin {
   }
 
   *#findPossibleBlocks(): IterableIterator<ContainerElement> {
-    const root = this.editor.editing.view.document.getRoot()!;
-    const range = this.editor.editing.view.createRangeIn(root);
+    const { mapper, view } = this.editor.editing;
+
+    const root = view.document.getRoot()!;
+    const range = view.createRangeIn(root);
     for (const value of range.getWalker({ ignoreElementEnd: true })) {
       const node = value.item;
       if (!node.is("containerElement")) {
         continue;
       }
 
-      const model = this.editor.editing.mapper.toModelElement(node);
+      const model = mapper.toModelElement(node);
 
       // Certain block elements such as `<ul>` or `<ol>` are inserted into the
       // view, but are not actually part of the model because there are only
