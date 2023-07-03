cacc538a202183529a644711380c3c0bc3524ad9
Alexander Ebert <ebert@woltlab.com>
2023-07-03 15:13:11 +0200

    Suppress the image toolbar for smileys
    Fixes #65
    See https://www.woltlab.com/community/thread/300482-smileys-werden-als-bilder-behandelt-und-sind-entsprechend-formatierbar/

--- plugins/ckeditor5-woltlab-smiley/src/woltlabsmiley.ts
+++ plugins/ckeditor5-woltlab-smiley/src/woltlabsmiley.ts
@@ -8,7 +8,10 @@
  */
 
 import { Plugin } from "@ckeditor/ckeditor5-core";
+import type { DowncastInsertEvent } from "@ckeditor/ckeditor5-engine";
 import { Image } from "@ckeditor/ckeditor5-image";
+
+import "../theme/woltlabsmiley.css";
 
 export class WoltlabSmiley extends Plugin {
   static get pluginName() {
@@ -36,6 +39,34 @@ export class WoltlabSmiley extends Plugin {
         key: "class",
         value: ["smiley"],
       },
+    });
+
+    conversion.for("editingDowncast").add((dispatcher) => {
+      dispatcher.on<DowncastInsertEvent>(
+        "insert:imageInline",
+        (_evt, data, conversionApi) => {
+          const { item } = data;
+
+          if (item.getAttribute("classList") !== "smiley") {
+            return;
+          }
+
+          if (!item.is("element")) {
+            return;
+          }
+
+          const { mapper, writer } = conversionApi;
+
+          // Inline images are widgetized which adds an outline when interacting
+          // with, breaks the caret movement and offers a toolbar to modify the
+          // positionining. Unfortunately, it is not possible to suppress this
+          // behavior, but everything depends on the custom property flag "widget"
+          // to check if it should offer any of these capabilities.
+          const viewElement = mapper.toViewElement(item)!;
+          writer.setCustomProperty("widget", false, viewElement);
+        },
+        { priority: "low" }
+      );
     });
   }
 }
