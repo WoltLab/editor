ba3ac0ca4cc0689094da97052d070fa6735ee715
Cyperghost <olaf_schmitz_1@t-online.de>
2024-10-18 12:13:18 +0200

    Add Emoji button to toolbar

--- plugins/ckeditor5-woltlab-emoji/src/woltlabemoji.ts
+++ plugins/ckeditor5-woltlab-emoji/src/woltlabemoji.ts
@@ -4,10 +4,81 @@
  * @license   LGPL-2.1-or-later
  * @since     6.2
  */
-import { Plugin } from "@ckeditor/ckeditor5-core";
-import { Database } from "emoji-picker-element";
+import { Plugin, Editor } from "@ckeditor/ckeditor5-core";
+import { Database, Picker } from "emoji-picker-element";
+import { Typing } from "@ckeditor/ckeditor5-typing";
+import { createDropdown } from "@ckeditor/ckeditor5-ui";
 
-export class WoltlabEmoji extends Plugin {}
+import emojiIcon from "../theme/icons/smile.svg";
+import WoltlabCoreEmojiPickerView from "./ui/woltlabcoreemojipickerview";
+import { EventInfo } from "@ckeditor/ckeditor5-utils";
+import { EmojiClickEvent } from "emoji-picker-element/shared";
+
+export class WoltlabEmoji extends Plugin {
+  #emojiPicker?: Picker = undefined;
+
+  constructor(editor: Editor) {
+    super(editor);
+  }
+
+  public static get pluginName() {
+    return "WoltlabEmoji" as const;
+  }
+
+  public static get requires() {
+    return [Typing] as const;
+  }
+
+  public init(): void {
+    const editor = this.editor;
+
+    const inputCommand = editor.commands.get("input")!;
+
+    editor.ui.componentFactory.add("WoltlabEmoji", (locale) => {
+      const dropdownView = createDropdown(locale);
+      dropdownView.buttonView.set({
+        label: editor.t("Emoji"),
+        icon: emojiIcon,
+        tooltip: true,
+      });
+      dropdownView.bind("isEnabled").to(inputCommand);
+
+      const emojiPickerView = new WoltlabCoreEmojiPickerView(locale);
+      this.listenTo(
+        emojiPickerView,
+        "emoji-click",
+        this.#emojiClicked.bind(this),
+      );
+
+      if (!emojiPickerView.isRendered) {
+        emojiPickerView.render();
+      }
+      this.#registerEmojiPicker(emojiPickerView.element!);
+
+      dropdownView.panelView.children.add(emojiPickerView);
+
+      return dropdownView;
+    });
+  }
+
+  #emojiClicked(evt: EventInfo, emojiClickData: EmojiClickEvent) {
+    const editor = this.editor;
+
+    if ("unicode" in emojiClickData.detail) {
+      editor.execute("input", { text: emojiClickData.detail.unicode });
+    }
+
+    editor.editing.view.focus();
+  }
+
+  #registerEmojiPicker(emojiPicker: Picker) {
+    if (this.#emojiPicker !== undefined) {
+      return;
+    }
+
+    this.#emojiPicker = emojiPicker;
+  }
+}
 
 export type WoltlabEmojiConfig = {
   getDatabase(): Database;
@@ -15,6 +86,6 @@ export type WoltlabEmojiConfig = {
 
 declare module "@ckeditor/ckeditor5-core" {
   interface EditorConfig {
-    woltlabEmojis: WoltlabEmojiConfig;
+    woltlabEmojis?: WoltlabEmojiConfig;
   }
 }
