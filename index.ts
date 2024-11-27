54d7052a834e3b41e5fdacd8a17a4a4ea1b6e76c
Cyperghost <olaf_schmitz_1@t-online.de>
2024-11-27 09:47:52 +0100

    Always loads the `types.ts` to allow the loading of `svg` files.

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@
  * @license LGPL-2.1-or-later
  * @since 6.0
  */
+// The import prevents the error `Cannot find module '….svg' or its corresponding type declarations.` from occurring if the editor is included as a dependency.
+import "./types";
 
 export { create } from "./app";
 export * as CKEditor5 from "./modules";
